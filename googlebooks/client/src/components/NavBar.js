9de54f7a84b83d3f36dbd4c1e37763098a15de30
Dylan Thomas <thomasdylan76@gmail.com>
2020-01-11 20:27:26 -0700

    Bootstrap CDN added and Navbar created.

---
+++ googlebooks/client/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                <img src="https://upload.wikimedia.org/wikipedia/en/b/ba/Google_Books_logo_2015.svg" width="200" height="40" alt="google books"/>
+                </a>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-item nav-link" href="#">Search</a>
+                        <a className="nav-item nav-link" href="#">Saved</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
