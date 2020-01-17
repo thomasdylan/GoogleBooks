f80358808c5c8d5c01234c8df5704585cedafdbf
Dylan Thomas <thomasdylan76@gmail.com>
2020-01-16 18:30:48 -0700

    Linked mLab

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://thomasdylan76:@T9RuAGGwwjFq6c@ds263928.mlab.com:63928/heroku_sgxwkd8x");
 
 // Start the API server
 app.listen(PORT, function () {
